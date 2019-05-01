f3724ad3137abfe2d573ade53de4247cff3dc183
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-01 13:20:46 +0000

    Deploy Michael-F-Bryan/libsignal-protocol-rs to github.com/Michael-F-Bryan/libsignal-protocol-rs.git:gh-pages

--- libsignal_protocol/sidebar-items.js
+++ libsignal_protocol/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["InternalError",""]],"struct":[["Context",""],["DefaultCrypto",""]],"trait":[["Crypto",""]]});
+initSidebarItems({"enum":[["InternalError",""]],"struct":[["Context",""],["DefaultCrypto",""],["IdentityKeyPair",""],["PreKeyList",""]],"trait":[["Crypto","Cryptography routines used in the signal protocol."]]});
