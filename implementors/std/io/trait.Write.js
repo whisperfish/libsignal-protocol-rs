9db81b4a0c51c9686d00839f0003535aaecab9b6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-08 15:45:02 +0000

    Deploy Michael-F-Bryan/libsignal-protocol-rs to github.com/Michael-F-Bryan/libsignal-protocol-rs.git:gh-pages

--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["libsignal_protocol"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"libsignal_protocol/struct.Buffer.html\" title=\"struct libsignal_protocol::Buffer\">Buffer</a>",synthetic:false,types:["libsignal_protocol::buffer::Buffer"]},];
+implementors["smallvec"] = [{text:"impl&lt;A:&nbsp;<a class=\"trait\" href=\"smallvec/trait.Array.html\" title=\"trait smallvec::Array\">Array</a>&lt;Item = <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>&gt;&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"smallvec/struct.SmallVec.html\" title=\"struct smallvec::SmallVec\">SmallVec</a>&lt;A&gt;",synthetic:false,types:["smallvec::SmallVec"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
