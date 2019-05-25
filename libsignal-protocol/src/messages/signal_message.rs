a69bbdb34b9a8c96fbb238bc61c0bd0bc1fafeac
Michael Bryan <michaelfbryan@gmail.com>
2019-05-25 20:25:15 +0800

    Added methods to SignalMessage

--- libsignal-protocol/src/messages/signal_message.rs
+++ libsignal-protocol/src/messages/signal_message.rs
@@ -1,6 +1,9 @@
 use crate::{
+    errors::InternalError,
+    keys::PublicKey,
     messages::{CiphertextMessage, CiphertextType},
     raw_ptr::Raw,
+    Context,
 };
 use failure::Error;
 use std::convert::TryFrom;
@@ -9,6 +12,73 @@ use std::convert::TryFrom;
 #[derive(Debug, Clone)]
 pub struct SignalMessage {
     pub(crate) raw: Raw<sys::signal_message>,
+}
+
+impl SignalMessage {
+    pub fn is_legacy(msg: &[u8]) -> bool {
+        unsafe {
+            sys::signal_message_is_legacy(msg.as_ptr(), msg.len()) != 0
+        }
+    }
+
+    pub fn sender_ratchet_key(&self) -> PublicKey {
+        unsafe {
+            let raw = sys::signal_message_get_sender_ratchet_key(
+                self.raw.as_const_ptr(),
+            );
+            PublicKey {
+                raw: Raw::copied_from(raw),
+            }
+        }
+    }
+
+    pub fn message_version(&self) -> u8 {
+        unsafe {
+            sys::signal_message_get_message_version(self.raw.as_const_ptr())
+        }
+    }
+
+    pub fn counter(&self) -> u32 {
+        unsafe { sys::signal_message_get_counter(self.raw.as_const_ptr()) }
+    }
+
+    pub fn body(&self) -> &[u8] {
+        unsafe {
+            let buffer = sys::signal_message_get_body(self.raw.as_const_ptr());
+            assert!(!buffer.is_null());
+
+            let len = sys::signal_buffer_len(buffer);
+            let data = sys::signal_buffer_data(buffer);
+
+            std::slice::from_raw_parts(data, len)
+        }
+    }
+
+    pub fn verify_mac(
+        &self,
+        sender_identity_key: &PublicKey,
+        receiver_identity_key: &PublicKey,
+        mac: &[u8],
+        ctx: &Context,
+    ) -> Result<bool, InternalError> {
+        unsafe {
+            let code = sys::signal_message_verify_mac(
+                self.raw.as_ptr(),
+                sender_identity_key.raw.as_ptr(),
+                receiver_identity_key.raw.as_ptr(),
+                mac.as_ptr(),
+                mac.len(),
+                ctx.raw(),
+            );
+
+            match code {
+                0 => Ok(false),
+                1 => Ok(true),
+                other => Err(InternalError::from_error_code(other)
+                    .unwrap_or(InternalError::Unknown)),
+            }
+        }
+    }
 }
 
 impl TryFrom<CiphertextMessage> for SignalMessage {
