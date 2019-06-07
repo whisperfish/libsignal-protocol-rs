162fbbd38918e6eb8721d9aaab608d2d312feb07
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-07 16:14:31 +0000

    Deploy Michael-F-Bryan/libsignal-protocol-rs to github.com/Michael-F-Bryan/libsignal-protocol-rs.git:gh-pages

--- libsignal_protocol/messages/sidebar-items.js
+++ libsignal_protocol/messages/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["CiphertextType",""]],"struct":[["CiphertextMessage","An encrypted message (\"ciphertext\")."],["PreKeySignalMessage","A message containing everything necessary to establish a session."],["SignalMessage","The base message type."]]});
+initSidebarItems({"enum":[["CiphertextType","The type of ciphertext message."]],"struct":[["CiphertextMessage","The base class for a ciphertext message."],["PreKeySignalMessage","A message containing everything necessary to establish a session."],["SignalMessage","A message with an arbitrary payload."]]});
