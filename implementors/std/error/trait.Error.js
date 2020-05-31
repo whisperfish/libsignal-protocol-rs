e56868e90d66132e242ed9e5bb19bbc956e74f9d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-31 07:19:58 +0000

    Deploy Michael-F-Bryan/libsignal-protocol-rs to github.com/Michael-F-Bryan/libsignal-protocol-rs.git:gh-pages

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -7,10 +7,4 @@ implementors["rand"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust
 implementors["rand_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"rand_core/struct.Error.html\" title=\"struct rand_core::Error\">Error</a>","synthetic":false,"types":["rand_core::error::Error"]}];
 implementors["rand_jitter"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"rand_jitter/enum.TimerError.html\" title=\"enum rand_jitter::TimerError\">TimerError</a>","synthetic":false,"types":["rand_jitter::error::TimerError"]}];
 implementors["syn"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"syn/struct.Error.html\" title=\"struct syn::Error\">Error</a>","synthetic":false,"types":["syn::error::Error"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
