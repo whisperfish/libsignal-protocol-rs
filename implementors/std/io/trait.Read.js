e972355fcb943a8fe85facd3ef2dbd3434eaa0bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-24 14:31:59 +0000

    Deploy Michael-F-Bryan/libsignal-protocol-rs to github.com/Michael-F-Bryan/libsignal-protocol-rs.git:gh-pages

--- implementors/std/io/trait.Read.js
+++ implementors/std/io/trait.Read.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
-implementors["rand_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for dyn <a class=\"trait\" href=\"rand_core/trait.RngCore.html\" title=\"trait rand_core::RngCore\">RngCore</a>","synthetic":false,"types":["rand_core::RngCore"]}];
+implementors["base64"] = [{"text":"impl&lt;'a, R:&nbsp;Read&gt; Read for DecoderReader&lt;'a, R&gt;","synthetic":false,"types":[]}];
+implementors["rand_core"] = [{"text":"impl Read for dyn RngCore","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
