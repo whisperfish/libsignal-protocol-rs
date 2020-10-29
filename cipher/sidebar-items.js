0e907d5b32c77978bf3c909fdd8d4bcf99ac73c0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-29 09:35:18 +0000

    Deploy Michael-F-Bryan/libsignal-protocol-rs to github.com/Michael-F-Bryan/libsignal-protocol-rs.git:gh-pages

--- cipher/sidebar-items.js
+++ cipher/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["block","Traits used to define functionality of block ciphers."],["consts","Type aliases for many constants."],["stream","Traits which define functionality of stream ciphers."]]});
+initSidebarItems({"mod":[["block","Traits used to define functionality of [block ciphers][1]."],["consts","Type aliases for many constants."],["stream","Traits which define functionality of stream ciphers."]]});
